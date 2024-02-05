d67e7b6f358e50e21299cbfc073fa513549ff2a3
pravin126 <pravinkalokhe5@.com>
2024-02-05 12:31:15 +0530

    add html for loadd cat and sub cat

--- E_shop/E_shop/views.py
+++ E_shop/E_shop/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from shop.models import category,subcategory
 
 def master(request):
     return render(request,'master.html')
 
 def index(request):
-    return render(request,'index.html')
+    cat=category.objects.all()
+    context={
+        "cat":cat,
+    }
+    return render(request,'index.html',context)
--- E_shop/shop/models.py
+++ E_shop/shop/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class category(models.Model):
     name=models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 
 class subcategory(models.Model):
     name=models.CharField(max_length=150)
     category=models.ForeignKey(category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
