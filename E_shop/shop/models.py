35c6288e66dc043be65d8be04a8a0b9c4e68ce81
pravin126 <pravinkalokhe5@.com>
2024-02-05 11:23:16 +0530

    create model in cat and subcat

--- E_shop/shop/models.py
+++ E_shop/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class category(models.Model):
+    name=models.CharField(max_length=150)
+
+
+class subcategory(models.Model):
+    name=models.CharField(max_length=150)
+    category=models.ForeignKey(category,on_delete=models.CASCADE)
